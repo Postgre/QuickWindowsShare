34da608a50d1e71fe5327124b63e76deeb8d1bac
mtnking <will.frishe@gmail.com>
2016-01-18 15:34:14 -0500

    v1

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace SelectShare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbFiles = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbGroups = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbFiles
+            // 
+            this.lbFiles.FormattingEnabled = true;
+            this.lbFiles.Location = new System.Drawing.Point(12, 25);
+            this.lbFiles.Name = "lbFiles";
+            this.lbFiles.Size = new System.Drawing.Size(349, 225);
+            this.lbFiles.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnOpenFiles_Click);
+            // 
+            // lbGroups
+            // 
+            this.lbGroups.FormattingEnabled = true;
+            this.lbGroups.Location = new System.Drawing.Point(419, 25);
+            this.lbGroups.Name = "lbGroups";
+            this.lbGroups.Size = new System.Drawing.Size(207, 225);
+            this.lbGroups.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(462, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnADOP_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Files";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(121, 256);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Remove";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.remUGs_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(440, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Users / Groups";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(533, 256);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Remove";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 294);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Share Name:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(419, 326);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Share";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.share_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(500, 326);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Cancel";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Location = new System.Drawing.Point(121, 320);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(212, 33);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            //this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 10);
+            this.radioButton3.Name = "radioButton1";
+            this.radioButton3.Size = new System.Drawing.Size(73, 17);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Copy Files";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(94, 10);
+            this.radioButton4.Name = "radioButton2";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Move Files";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 365);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbGroups);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbFiles);
+            this.Name = "Form1";
+            this.Text = "SelectShare";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbFiles;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lbGroups;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Security.Permissions;
+using System.Security;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using Tulpep.ActiveDirectoryObjectPicker;
+
+namespace SelectShare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton3.Checked = true;
+        }
+        private void btnOpenFiles_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                    lbFiles.Items.Add((filename));
+            }
+        }
+
+        private void btnADOP_Click(object sender, EventArgs e)
+        {
+            DirectoryObjectPickerDialog picker = new DirectoryObjectPickerDialog()
+            {
+                AllowedObjectTypes = ObjectTypes.Groups | ObjectTypes.Users,
+                DefaultObjectTypes = ObjectTypes.Groups | ObjectTypes.Users,
+                AllowedLocations = Locations.All,
+                DefaultLocations = Locations.JoinedDomain,
+                MultiSelect = true,
+                ShowAdvancedView = true
+            };
+
+            try
+            {
+                if (picker.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (var sel in picker.SelectedObjects)
+                    {
+                        lbGroups.Items.Add(sel.Name);
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void remFiles_Click(object sender, EventArgs e)
+        {
+            for (int x = lbFiles.SelectedIndices.Count - 1; x >= 0; x--)
+            {
+                int idx = lbFiles.SelectedIndices[x];
+                lbFiles.Items.RemoveAt(idx);
+            }
+        }
+
+        private void remUGs_Click(object sender, EventArgs e)
+        {
+            for (int x = lbGroups.SelectedIndices.Count - 1; x >= 0; x--)
+            {
+                int idx = lbGroups.SelectedIndices[x];
+                lbGroups.Items.RemoveAt(idx);
+            }
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private ManagementObject genUGSec(string ugname)
+        {
+            NTAccount ntAccount = new NTAccount(ugname);
+            SecurityIdentifier oGrpSID = (SecurityIdentifier)ntAccount.Translate(typeof(SecurityIdentifier));
+            byte[] utenteSIDArray = new byte[oGrpSID.BinaryLength];
+            oGrpSID.GetBinaryForm(utenteSIDArray, 0);
+            ManagementObject oGrpTrustee = new ManagementClass(new ManagementPath("Win32_Trustee"), null);
+            oGrpTrustee["Name"] = ugname;
+            oGrpTrustee["SID"] = utenteSIDArray;
+            ManagementObject oGrpACE = new ManagementClass(new ManagementPath("Win32_Ace"), null);
+            oGrpACE["AccessMask"] = 2032127;//Full access
+            oGrpACE["AceFlags"] = AceFlags.ObjectInherit | AceFlags.ContainerInherit; //propagate the AccessMask to the subfolders
+            oGrpACE["AceType"] = AceType.AccessAllowed;
+            oGrpACE["Trustee"] = oGrpTrustee;
+            return oGrpACE;
+        }
+
+        private void share_Click(object sender, EventArgs e)
+        {
+            string sharepath = "C:\\Shares\\";
+            if (lbFiles.Items.Count == 0)
+            {
+                MessageBox.Show("You must select files to share.", "SelectShare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (lbGroups.Items.Count == 0)
+            {
+                MessageBox.Show("You must select users/groups with whom to share.", "SelectShare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("You must select a unique share name.", "SelectShare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string sname = sharepath + textBox1.Text;
+            if(Directory.Exists(sname)) {
+                MessageBox.Show("Chosen share name is in use. Please choose another.", "SelectShare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+            Directory.CreateDirectory(sname);
+
+            var choice = groupBox1.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked);
+            foreach (var fn in lbFiles.Items)
+            {
+                if (choice == radioButton3)
+                    File.Copy(fn.ToString(), sname+"\\"+Path.GetFileName(fn.ToString())); 
+                    
+                if(choice == radioButton4)
+                    File.Move(fn.ToString(), sname + "\\" + Path.GetFileName(fn.ToString()));
+            }
+            try
+            {
+                ManagementObject oGrpSecurityDescriptor = new ManagementClass(new ManagementPath("Win32_SecurityDescriptor"), null);
+                oGrpSecurityDescriptor["ControlFlags"] = 4; //SE_DACL_PRESENT
+                var otmp = new object[lbGroups.Items.Count];
+                int i = 0;
+                foreach (var ug in lbGroups.Items)
+                {
+                    otmp[i] = genUGSec(ug.ToString());
+                    i++;
+                }
+                oGrpSecurityDescriptor["DACL"] = otmp;
+                ManagementClass mc = new ManagementClass("Win32_Share");
+                ManagementBaseObject inParams = mc.GetMethodParameters("Create");
+                ManagementBaseObject outParams;
+                inParams["Description"] = textBox1.Text;
+                inParams["Name"] = textBox1.Text;
+                inParams["Path"] = sname;
+                inParams["Type"] = 0x0; // Disk Drive\ inParams["MaximumAllowed"] = null;
+                inParams["Password"] = null;
+                inParams["Access"] = oGrpSecurityDescriptor; 
+                outParams = mc.InvokeMethod("Create", inParams, null);
+                if ((uint)(outParams.Properties["ReturnValue"].Value) != 0)
+                {
+                    throw new Exception("Unable to share directory.");
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); return; }
+        }
+
+    }
+}
